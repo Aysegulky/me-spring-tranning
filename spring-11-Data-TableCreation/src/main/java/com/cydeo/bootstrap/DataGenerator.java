57f903e1a465fcabbf16026897aac825a29366d8
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-09-27 18:45:08 +0300

    Spring data Part 2 Video 16 continue

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,16 +1,34 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Car;
+import com.cydeo.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
+
+
+    private final CarRepository carRepository;
+
+    public DataGenerator(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Car car1 = new Car("BMV", "M3");
-        Car car2 = new Car("Honda", "Civic");
-        Car car3 = new Car("Toyota", "Corolla");
+        Car c1 = new Car("BMV", "M3");
+        Car c2 = new Car("Honda", "Civic");
+        Car c3 = new Car("Toyota", "Corolla");
+
+        //i want to save c1/c2/c3 to db
+
+        //do we any method which is doing this saving for me??? yeees... whwre? inside the CarRepository  same method
+
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
 
 
 
