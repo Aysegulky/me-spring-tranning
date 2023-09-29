5f89346fe0f06e036641d8100dd2d68fb10bd015
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-09-29 10:33:38 +0300

    Spring data Video 8 CinemaLab

--- spring-13-Data-LabCinema/src/main/java/com/cydeo/entity/Ticket.java
+++ spring-13-Data-LabCinema/src/main/java/com/cydeo/entity/Ticket.java
@@ -1,4 +1,26 @@
 package com.cydeo.entity;
 
-public class Ticket {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Ticket extends BaseEntity {
+
+    private Integer seatNumber;
+    private Integer rowNumber;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieCinema movieCinema;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User userAccount;
+
 }
